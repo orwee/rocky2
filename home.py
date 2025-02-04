61279c219fe0c5a9204d26f93da26d2649b051da
orwee <orweebusiness@gmail.com>
2025-02-04 20:45:47 +0100

    Update home.py

--- home.py
+++ home.py
@@ -6,14 +6,6 @@ def main():
     st.set_page_config(page_title="Mi Agente DeFi - Chat", layout="wide")
     st.title("Chat DeFi")
 
-        # Inicializar session_state si no existe
-    if 'combined_df' not in st.session_state:
-        st.session_state['combined_df'] = None
-    if 'portfolio_summary' not in st.session_state:
-        st.session_state['portfolio_summary'] = None
-    if 'messages' not in st.session_state:
-        st.session_state['messages'] = []
-
     init_chat_history()
     render_chat()
 
